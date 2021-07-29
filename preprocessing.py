e1a544d93b05ed359a138f006fdee05048c1fa38
Nima Mojtahedi <nima.mojtahedigm@gmail.com>
2021-07-29 13:14:29 +0200

    updaating util and preprocessing files with some basic functions such as reading function (at the moment scripts are simple functions but in future they could change to class)

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,33 @@
 # All prerpocessing functions are provided in this file
+
+
+def pre_processing():
+    """
+    preprocessing class takes few parameters as input and apply requested
+    preprocessing steps on data
+    methods are wraper around sklearn package preprocessing module
+
+    data: input data (sample * dimension)
+    method: name of preprocessing method needs to be applied
+            'normalization'
+            'standarization'
+            'minmax'
+            'Kbins_discretization'
+            'polynomial'
+                .
+                .
+                .
+
+    """
+    # importing sklearn preprocessing module
+    from sklearn import preprocessing
+
+    return preprocessing
+
+    def IO():
+        """
+        This is wraper function around MNE input/oupt modules
+        """
+        from mne import io
+
+        return io
--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
 # All helper functions are written in this file.
+
+def IO():
+    """
+        This is wraper function around MNE input/oupt modules
+        """
+    from mne import io
+
+    return io
